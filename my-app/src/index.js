ccd389a0674d16d6d21ab087f0dbdde535bcc727
lien-eckang <euncho.kang@lien.kr>
2022-06-16 11:48:07 +0900

    Square 변경
     - Square의 render 함수 내부의 this.state.value를 this.props.value로 바꾸어주세요.
     - Square의 render 함수 내부의 this.setState()를 this.props.onClick()으로 바꾸어주세요.
     - Square는 게임의 상태를 유지할 필요가 없기 때문에 constructor를 지워주세요.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
