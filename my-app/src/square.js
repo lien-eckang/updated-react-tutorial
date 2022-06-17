647ea7446cb2e7ee7b326134b0977c3849d00514
lien-eckang <euncho.kang@lien.kr>
2022-06-17 11:31:16 +0900

    refactoring board.js
    - board.js를 refactoring

--- my-app/src/square.js
+++ my-app/src/square.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export  default class Square extends React.Component {
+export class Square extends React.Component {
     render() {
       return (
       <button className="square" onClick={() => this.props.onClick()}>
